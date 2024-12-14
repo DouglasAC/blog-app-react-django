9dae9ec4ec1f973d193444e2efd8a0a5d90702f6
DouglasAC <3049786550116@ingenieria.usac.edu.gt>
2024-12-14 15:58:22 -0600

    Add signal handlers to create default categories and tags on post-migrate

---
+++ blog_backend/blog/signals.py
@@ -0,0 +1,57 @@
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from .models import Category, Tag
+
+DEFAULT_CATEGORIES = [
+    "Tecnología",
+    "Ciencia",
+    "Arte y Cultura",
+    "Salud",
+    "Estilo de Vida",
+    "Negocios",
+    "Educación",
+    "Deportes",
+    "Noticias"
+]
+
+@receiver(post_migrate)
+def create_default_categories(sender, **kwargs):
+    if sender.name == "blog":
+        for category in DEFAULT_CATEGORIES:
+            Category.objects.get_or_create(name=category, slug=category.lower().replace(" ", "-"))
+
+DEFAULT_TAGS = [
+    "Python",
+    "Django",
+    "IA",
+    "Ciberseguridad",
+    "Desarrollo Web",
+    "Meditación",
+    "Ejercicio",
+    "Recetas",
+    "Cine",
+    "Pintura",
+    "Música",
+    "Viajes",
+    "Decoración",
+    "Aprendizaje",
+    "Métodos de Estudio",
+    "Cursos"
+    "Marketing",
+    "Finanzas",
+    "Fútbol",
+    "Baloncesto",
+    "Eventos",
+    "Espacio Exterior",
+    "Medio Ambiente",
+    "Inpiración",
+    "Noticias del Mundo"
+    "Historias cortas",
+    "Cuentos"
+]
+
+@receiver(post_migrate)
+def create_default_tags(sender, **kwargs):
+    if sender.name == "blog":
+        for tag in DEFAULT_TAGS:
+            Tag.objects.get_or_create(name=tag, slug=tag.lower().replace(" ", "-"))
